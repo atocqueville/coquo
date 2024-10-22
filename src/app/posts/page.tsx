8cb1629ffd252c33bad025a156e06f6f79edcab7
Alexandre de Tocqueville <alex.detocqueville@gmail.com>
2024-10-22 14:20:54 +0200

    [Tech](Tooling) isolate prisma schema and seed

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,7 +4,7 @@ import ButtonLink from '@/components/button-link'
 
 async function getData(): Promise<Post[]> {
     return await prisma.post.findMany({
-        //   where: { published: true },
+        // where: { published: true },
         include: {
             author: {
                 select: { email: true },
@@ -24,16 +24,15 @@ export default async function Page() {
             <ButtonLink path="/create">Create a new post !</ButtonLink>
 
             {posts.map((post) => (
-                <p key={post.id}>
+                <div
+                    key={post.id}
+                    className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                >
                     <div>
-                        <p>
-                            Make beautiful websites regardless of your design
-                            experience.
-                        </p>
                         <p>{post.title}</p>
                         <p>{post.content}</p>
                     </div>
-                </p>
+                </div>
             ))}
         </>
     )
