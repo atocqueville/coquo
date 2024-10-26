01ac4e37536f4bb09e7c696a534f6e3f0a5a6bf9
Alexandre de Tocqueville <alex.detocqueville@gmail.com>
2024-10-26 16:58:17 +0200

    restaure auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
-import NextAuth, { type NextAuthConfig } from 'next-auth'
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import prisma from '@/lib/prisma'
-import Credentials from 'next-auth/providers/credentials'
-import { z } from 'zod'
-import { compare } from 'bcryptjs'
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/lib/prisma';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { compare } from 'bcryptjs';
 
 export const authConfig: NextAuthConfig = {
     trustHost: true,
@@ -11,9 +11,10 @@ export const authConfig: NextAuthConfig = {
         strategy: 'jwt',
     },
     callbacks: {
+        authorized: async ({ auth }) => !!auth,
         jwt: ({ token, user }) => {
-            if (user) token.id = user.id
-            return token
+            if (user) token.id = user.id;
+            return token;
         },
         session: ({ session, token }) => {
             return {
@@ -22,17 +23,13 @@ export const authConfig: NextAuthConfig = {
                     ...session.user,
                     id: token.id as string,
                 },
-            }
+            };
         },
     },
     adapter: PrismaAdapter(prisma),
     providers: [
         Credentials({
             name: 'Credentials',
-            // `credentials` is used to generate a form on the sign in page.
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
             credentials: {
                 email: { label: 'User E-mail', type: 'text' },
                 password: { label: 'Password', type: 'password' },
@@ -43,26 +40,26 @@ export const authConfig: NextAuthConfig = {
                         email: z.string().email(),
                         password: z.string().min(6),
                     })
-                    .safeParse(credentials)
+                    .safeParse(credentials);
                 if (parsedCredentials.success) {
-                    const { email, password } = parsedCredentials.data
+                    const { email, password } = parsedCredentials.data;
 
                     const user = await prisma.user.findUnique({
                         where: { email },
-                    })
+                    });
 
-                    if (!user) return null
+                    if (!user) return null;
                     const passwordsMatch = await compare(
                         password,
                         user.password || ''
-                    )
-                    if (passwordsMatch) return user
+                    );
+                    if (passwordsMatch) return user;
                 }
-                console.log('Invalid credentials')
-                return null
+                console.log('Invalid credentials');
+                return null;
             },
         }),
     ],
-}
+};
 
-export const { auth, signIn, signOut, handlers } = NextAuth(authConfig)
+export const { auth, signIn, signOut, handlers } = NextAuth(authConfig);
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,10 @@
-import {
-    NextResponse,
-    type MiddlewareConfig,
-    type NextRequest,
-} from 'next/server'
-import { auth } from '@/auth'
+import { type MiddlewareConfig } from 'next/server'
 
-// export default NextAuth(authConfig).auth
-const protectedRoutes = ['/posts', '/create']
-
-export default async function middleware(req: NextRequest) {
-    const session = await auth()
-    const isProtected = protectedRoutes.some((route) =>
-        req.nextUrl.pathname.startsWith(route)
-    )
-    if (!session && isProtected) {
-        const absoluteUrl = new URL('/api/auth/signin', req.nextUrl.origin)
-        return NextResponse.redirect(absoluteUrl)
-    }
-
-    return NextResponse.next()
-}
+export { auth as middleware } from '@/auth'
 
 export const config: MiddlewareConfig = {
     // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+    matcher: [
+        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    ],
 }
