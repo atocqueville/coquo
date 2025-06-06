4a3864e0648613c91cf7a8c828f5193c77931606
Alexandre de Tocqueville <alex.detocqueville@gmail.com>
2025-06-06 14:45:01 +0200

    recipe id page localized

--- src/app/(home)/r/[id]/components/back-button.tsx
+++ src/app/(home)/r/[id]/components/back-button.tsx
@@ -3,9 +3,11 @@
 import { Button } from '@/components/ui/button';
 import { ChevronLeft } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { useTranslations } from 'next-intl';
 
 export default function BackButton() {
     const router = useRouter();
+    const t = useTranslations('RecipePage.actions');
 
     return (
         <div className="absolute top-4 left-4 z-20">
@@ -16,7 +18,7 @@ export default function BackButton() {
                 className="rounded-full shadow-lg"
             >
                 <ChevronLeft className="h-4 w-4" />
-                <span className="sr-only">Retour à la liste des recettes</span>
+                <span className="sr-only">{t('backToList')}</span>
             </Button>
         </div>
     );
